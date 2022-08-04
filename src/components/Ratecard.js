a6a625542765759bcf2f8ce0dff6ac1f8177e2a3
timrusin <timrusin@gmail.com>
2022-08-03 21:08:07 -0500

    started working on rate cards on services page

---
+++ src/components/Ratecard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Ratecard.css'
+
+const Ratecard = () => {
+  return (
+    <div id="rate-card"></div>
+  )
+}
+
+export default Ratecard
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import './Services.css'
 import products from '../images/products-edit-low.mp4'
+import Ratecard from '../components/Ratecard';
 
 const Services = () => {
   return (
-    <div className='services-container'>
-      <video
+    <div className="services-container">
+      {/* <video
         src={products}
         autoPlay
         playsInline
         loop
         muted
         className="fade-in product-video"
-      />
-      <div className='services-title'>
+      /> */}
+      <div className="services-title">
         <h1>Services</h1>
+      </div>
+      <div className="test">
+        <Ratecard />
       </div>
     </div>
   );
