1ba91011e3a2d7dd2246956d8a2afca279d298b1
timrusin <timrusin@gmail.com>
2022-08-04 12:36:22 -0500

    fixed another warning

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Services.css'
-import products from '../images/products-edit-low.mp4'
 import Ratecard from '../components/Ratecard';
 
 const Services = () => {
