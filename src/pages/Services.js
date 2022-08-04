cb791f13466be501203ca2b91466c38fd76cf5d0
timrusin <timrusin@gmail.com>
2022-08-04 12:28:54 -0500

    working on services page and cards

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -6,14 +6,6 @@ import Ratecard from '../components/Ratecard';
 const Services = () => {
   return (
     <div className="services-container">
-      {/* <video
-        src={products}
-        autoPlay
-        playsInline
-        loop
-        muted
-        className="fade-in product-video"
-      /> */}
       <div className="services-title">
         <h1>Services</h1>
       </div>
