0757bf781c6464fb71b4582b40dd7d426f61236d
timrusin <timrusin@gmail.com>
2022-08-10 22:38:13 -0500

    initial stab at getting service cards to spin into screen

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import './Services.css'
 import Ratecard from '../components/Ratecard';
 
 const Services = () => {
+  const [cardOne, setCardOne]= useState(false)
+  const [cardTwo, setCardTwo]= useState(false)
+  const [cardThree, setCardThree]= useState(false)
+  const [cardFour, setCardFour]= useState(false)
+
+
+  useEffect(()=> {
+    const CardTwo = ()=>{
+      setCardTwo(true)
+      setTimeout(CardThree,400)
+    }
+    const CardThree = () => {
+      setCardThree(true)
+      setTimeout(CardFour, 400)
+    }
+    const CardFour = () => {
+      setCardFour(true)
+    }
+    setCardOne(true)
+    setTimeout(CardTwo,400)
+},[])
+
   return (
+
     <div className="services-container">
-      <div className="services-title">
-        <h1>Services</h1>
-      </div>
       <div className="cards">
-        <div className='card fade-in'><Ratecard /></div>
-        <div className='card fade-in'><Ratecard /></div>
-        <div className='card fade-in'><Ratecard /></div>
-        <div className='card fade-in'><Ratecard /></div>
+        <div className={cardOne ? "card-one" : "card-hide"}><Ratecard /></div>
+        <div className={cardTwo? "card-two" : "card-hide"}><Ratecard /></div>
+        <div className={cardThree ? "card-three" : "card-hide"}><Ratecard /></div>
+        <div className={cardFour ? "card-four" : "card-hide"}><Ratecard /></div>
       </div>
     </div>
   );
