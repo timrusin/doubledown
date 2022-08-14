e3b5f2cb28deb3c6c542e4501dfab675e34b204d
timrusin <timrusin@gmail.com>
2022-08-14 12:16:12 -0500

    working on random color for services card acrting weird though

--- src/components/Ratecard.js
+++ src/components/Ratecard.js
@@ -1,21 +1,23 @@
-import React from 'react'
 import './Ratecard.css'
 import {Link} from 'react-router-dom'
 
 const Ratecard = () => {
+  const colors = ['red', 'black']
+  const randomColor = Math.floor(Math.random()*2)
+
   return (
     <div className="rate-card inner-border">
       <div className="top-left">
-        <h1 className="D">D</h1>
-        <i className="fa-solid fa-heart"></i>
+        <h1 style={{color: colors[randomColor]}} className="D" >D</h1>
+        <i style={{color: colors[randomColor]}} className="fa-solid fa-heart" ></i>
       </div>
       <div className="content">
         <h1>Service Name</h1>
         <ul className="service-list">
-          <li>service 1 place holder</li>
-          <li>service 2 place holder</li>
-          <li>service 3 place holder</li>
-          <li>service 4 place holder</li>
+          <li>service 1 placeholder</li>
+          <li>service 2 placeholder</li>
+          <li>service 3 placeholder</li>
+          <li>service 4 placeholder</li>
         </ul>
         <Link to="/contact" className='quote-link'>GET A QUOTE</Link>
       </div>
