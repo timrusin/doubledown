4007402ae256f909e8114d5734d0866ae180a498
timrusin <timrusin@gmail.com>
2022-08-15 20:09:43 -0500

    added placeholder suits for spade and club made cards a bit bigger and added bottom suit again

--- src/components/Ratecard.js
+++ src/components/Ratecard.js
@@ -7,11 +7,12 @@ const Ratecard = () => {
   const [suit, setSuit] = useState('')
 
   useEffect(()=>{
-    const suits = ['fa-heart', 'fa-diamond']
+    const suits = ['fa-heart', 'fa-diamond', 'fa-droplet', 'fa-crown']
     const colors = ['red', 'black']
-    const randomIndex = Math.floor(Math.random()*2)
-    setColor(colors[randomIndex])
-    setSuit(suits[randomIndex])
+    const randomColor = Math.floor(Math.random()*2)
+    const randomSuit = Math.floor(Math.random()*4)
+    setColor(colors[randomColor])
+    setSuit(suits[randomSuit])
   },[])
 
   return (
@@ -29,6 +30,10 @@ const Ratecard = () => {
           <li>service 4 placeholder</li>
         </ul>
         <Link to="/contact" className='quote-link'>GET A QUOTE</Link>
+      </div>
+      <div className="bottom-right">
+        <h1 style={{color: color}} className="D" >D</h1>
+        <i style={{color: color}} className={`fa-solid ${suit}`} ></i>
       </div>
     </div>
   );
