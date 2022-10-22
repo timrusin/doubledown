57d742fd78255432df07e674c4f1a52dd37a2d6b
timrusin <timrusin@gmail.com>
2022-10-22 16:17:17 -0500

    started services data file

---
+++ src/ServicesData.js
@@ -0,0 +1,46 @@
+const ServicesData = [
+{
+    name: "Maintenance Wash",
+    bulletOne:"",
+    bulletTwo:"",
+    bulletThree:"",
+    bulletFour:"",
+    bulletFive:"",
+    bulletSix:"",
+    bulletSeven:""
+},
+{
+    name: "Premium Wash and Paint Correction",
+    bulletOne:"",
+    bulletTwo:"",
+    bulletThree:"",
+    bulletFour:"",
+    bulletFive:"",
+    bulletSix:"",
+    bulletSeven:"",
+    bulletEight:"",
+    butlletNine:""
+},
+{
+    name: "Maintenance Interior Detail",
+    bulletOne:"",
+    bulletTwo:"",
+    bulletThree:"",
+    bulletFour:"",
+    bulletFive:"",
+    bulletSix:""
+},
+{
+    name: "Premium Inerior Detail",
+    bulletOne:"",
+    bulletTwo:"",
+    bulletThree:"",
+    bulletFour:"",
+    bulletFive:"",
+    bulletSix:"",
+    bulletSeven:"",
+    bulletEight:"",
+    butlletNine:""
+}
+
+]
