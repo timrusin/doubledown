6bc4555ae9a18d9d16f18b4205e2064089f18c10
timrusin <timrusin@gmail.com>
2022-10-28 16:36:31 -0500

    added mobile arrows

--- src/components/BeforeAfter.js
+++ src/components/BeforeAfter.js
@@ -3,18 +3,22 @@ import './BeforeAfter.css'
 
 const BeforeAfter = () => {
   return (
-    <div className='before-after-container'>
-        <i class="fa-sharp fa-solid fa-arrow-left arrows"></i>
-        <div className='image'>
-            <img src='/assets/before_after_photos/wheel_before.jpeg' alt="" className='images'></img>
-            <h1 className='before-after'>BEFORE</h1>
+    <>
+    <i class="fa-sharp fa-solid fa-arrow-left arrows-mobile"></i>
+        <div className='before-after-container'>
+        <i class="fa-sharp fa-solid fa-arrow-left arrows-desktop"></i>
+            <div className='image'>
+                <img src='/assets/before_after_photos/wheel_before.jpeg' alt="" className='images'></img>
+                <h1 className='before-after'>BEFORE</h1>
+            </div>
+            <div className='image'>
+                <img src='/assets/before_after_photos/wheel_after.jpeg' alt="" className='images'></img>
+                <h1 className='before-after'>AFTER</h1>
+            </div>
+            <i class="fa-sharp fa-solid fa-arrow-right arrows-desktop"></i>
         </div>
-        <div className='image'>
-            <img src='/assets/before_after_photos/wheel_after.jpeg' alt="" className='images'></img>
-            <h1 className='before-after'>AFTER</h1>
-        </div>
-        <i class="fa-sharp fa-solid fa-arrow-right arrows"></i>
-    </div>
+        <i class="fa-sharp fa-solid fa-arrow-right arrows-mobile"></i>
+    </>
   )
 }
 
