0f0a152d99c996bda75db4712edd14bf0daa4fd2
timrusin <timrusin@gmail.com>
2022-10-28 16:40:27 -0500

    trying different images and arrows bigger

--- src/components/BeforeAfter.js
+++ src/components/BeforeAfter.js
@@ -8,11 +8,11 @@ const BeforeAfter = () => {
         <div className='before-after-container'>
         <i class="fa-sharp fa-solid fa-arrow-left arrows-desktop"></i>
             <div className='image'>
-                <img src='/assets/before_after_photos/wheel_before.jpeg' alt="" className='images'></img>
+                <img src='/assets/before_after_photos/interior_back_before.jpeg' alt="" className='images'></img>
                 <h1 className='before-after'>BEFORE</h1>
             </div>
             <div className='image'>
-                <img src='/assets/before_after_photos/wheel_after.jpeg' alt="" className='images'></img>
+                <img src='/assets/before_after_photos/interior_back_after.jpeg' alt="" className='images'></img>
                 <h1 className='before-after'>AFTER</h1>
             </div>
             <i class="fa-sharp fa-solid fa-arrow-right arrows-desktop"></i>
