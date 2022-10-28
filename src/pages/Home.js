bffdb01c63c8ec9e811e2c1a1da3e3bd6f41a993
timrusin <timrusin@gmail.com>
2022-10-27 23:19:03 -0500

    new home screen with about section displayed and removed from nav

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
+import About from './About'
 
 const Home = () => {
   return (
     <div className="home-container fade">
       <img src="/assets/medium_photos/lambo_front_medium.jpeg" alt="lambo"></img>
+      <About/>
     </div>
   );
 }
