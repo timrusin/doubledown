9adade758cf2f003234ed55d3557310612523111
timrusin <timrusin@gmail.com>
2022-10-28 20:53:50 -0500

    added interval to rotate through before and after photos

--- src/components/BeforeAfter.js
+++ src/components/BeforeAfter.js
@@ -1,23 +1,43 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import './BeforeAfter.css'
+import Data from '../data/BeforeAfterData'
 
 const BeforeAfter = () => {
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+        const interval = setInterval(() => {
+          setIndex(index+1)
+          if (index >= Data.length -1) setIndex(0)
+        }, 6000);
+        return () => clearInterval(interval);
+    });
+    console.log(index);
+    const previousPic = () => {
+      setIndex(index - 1)
+      if (index === 0) setIndex(Data.length - 1)
+    }
+
+    const nextPic = () => {
+      setIndex(index + 1)
+      if (index === Data.length - 1) setIndex(0)
+    }
   return (
     <>
-    <i class="fa-sharp fa-solid fa-arrow-left arrows-mobile"></i>
+    <i class="fa-sharp fa-solid fa-arrow-left arrows-mobile" onClick={previousPic}></i>
         <div className='before-after-container'>
-        <i class="fa-sharp fa-solid fa-arrow-left arrows-desktop"></i>
+        <i class="fa-sharp fa-solid fa-arrow-left arrows-desktop" onClick={previousPic}></i>
             <div className='image'>
-                <img src='/assets/before_after_photos/paint_correct_before.jpeg' alt="" className='images'></img>
+                <img src={Data[index].img_before} alt={Data[index].alt_before} className='images'></img>
                 <h1 className='before-after'>BEFORE</h1>
             </div>
             <div className='image'>
-                <img src='/assets/before_after_photos/paint_correct_after.jpeg' alt="" className='images'></img>
+                <img src={Data[index].img_after} alt={Data[index].alt_after} className='images'></img>
                 <h1 className='before-after'>AFTER</h1>
             </div>
-            <i class="fa-sharp fa-solid fa-arrow-right arrows-desktop"></i>
+            <i class="fa-sharp fa-solid fa-arrow-right arrows-desktop" onClick={nextPic}></i>
         </div>
-        <i class="fa-sharp fa-solid fa-arrow-right arrows-mobile"></i>
+        <i class="fa-sharp fa-solid fa-arrow-right arrows-mobile" onClick={nextPic}></i>
     </>
   )
 }
---
+++ src/data/BeforeAfterData.js
@@ -0,0 +1,61 @@
+const BeforeAfter = [
+    {
+        id: 1,
+        title: "Audi Suv",
+        img_before: "/assets/before_after_photos/audi_before_medium.jpeg",
+        alt_before: "Audi Suv before",
+        img_after: "/assets/before_after_photos/audi_after_medium.jpeg",
+        alt_after: "Audi Suv after"
+    },
+    {
+        id: 2,
+        title: "Back Inerior",
+        img_before: "/assets/before_after_photos/interior_back_before_medium.jpeg",
+        alt_before: "Interior back before",
+        img_after: "/assets/before_after_photos/interior_back_after_medium.jpeg",
+        alt_after: "Interior back after"
+    },
+    {
+        id: 3,
+        title: "Paint Correction",
+        img_before: "/assets/before_after_photos/paint_correct_before_medium.jpeg",
+        alt_before: "Paint correction before",
+        img_after: "/assets/before_after_photos/paint_correct_after_medium.jpeg",
+        alt_after: "Paint correction after"
+    },
+    {
+        id: 4,
+        title: "Tacoma Interior 1",
+        img_before: "/assets/before_after_photos/tacoma_int_before_medium.jpeg",
+        alt_before: "Tacoma interior 1 before",
+        img_after: "/assets/before_after_photos/tacoma_int_after_medium.jpeg",
+        alt_after: "Tacoma interior 1 after"
+    },
+    {
+        id: 5,
+        title: "Tacoma Interior 2",
+        img_before: "/assets/before_after_photos/tacoma_int_2_before_medium.jpeg",
+        alt_before: "Tacoma interior 2 before",
+        img_after: "/assets/before_after_photos/tacoma_int_2_after_medium.jpeg",
+        alt_after: "Tacoma interior 2 after"
+    },
+    {
+        id: 6,
+        title: "Tacoma Interior 3",
+        img_before: "/assets/before_after_photos/tacoma_int_3_before_medium.jpeg",
+        alt_before: "Tacoma interior 3 before",
+        img_after: "/assets/before_after_photos/tacoma_int_3_after_medium.jpeg",
+        alt_after: "Tacoma interior 3 after",
+    },
+    {
+        id: 7,
+        title: "Wheel Cleaning",
+        img_before: "/assets/before_after_photos/wheel_before_medium.jpeg",
+        alt_before: "Wheel cleaning before",
+        img_after: "/assets/before_after_photos/wheel_after_medium.jpeg",
+        alt_after: "Wheel cleaning after",
+    }
+    
+]
+
+export default BeforeAfter
