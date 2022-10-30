bc06846e8e8e279e45dc646456c3638f426e4b4b
timrusin <timrusin@gmail.com>
2022-10-29 20:08:12 -0500

    set cards back to original size for desktop, a lot more detail to go into for that move

---
+++ src/data/ShowcaseData.js
@@ -0,0 +1,108 @@
+const Showcase = [
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+    {
+        title: '',
+        img: '',
+        img_alt: '',
+        position: ''
+    },
+
+
+]
+
+export default Showcase
--- src/pages/Showcase.js
+++ src/pages/Showcase.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import ShowcaseData from '../data/ShowcaseData'
 
 const Showcase = () => {
-  return (
-    <div className="contact-container">
-      <h1 className="contact-title">THIS IS THE SHOWCASE PAGE</h1>
-      <h4>site currently in development</h4>
-    </div>
-  )
-}
+    const [index, setIndex] = useState(0)
+
+    useEffect(() => {
+          const interval = setInterval(() => {
+            setIndex(index+1)
+            if (index >= ShowcaseData.length -1) setIndex(0)
+          }, 6000);
+          return () => clearInterval(interval);
+      });
+      console.log(index);
+      const previousPic = () => {
+        setIndex(index - 1)
+        if (index === 0) setIndex(ShowcaseData.length - 1)
+      }
+  
+      const nextPic = () => {
+        setIndex(index + 1)
+        if (index === ShowcaseData.length - 1) setIndex(0)
+      }
+    return (
+      <div className= 'home-carousel fade'>
+      <img src={ShowcaseData[index].url} alt={ShowcaseData[index].alt} style={{objectPosition: ShowcaseData[index].position}} className={index % 2 !== 0 ? 'bg-image fade' : 'hide fade'}></img>
+      <img src={ShowcaseData[index].url} alt={ShowcaseData[index].alt} style={{objectPosition: ShowcaseData[index].position}} className={index % 2 === 0 ? 'bg-image fade' : 'hide fade'}></img>
+        <div className='nav-arrows-container'>
+          <i className="fa-thin fa-arrow-left arrows" onClick= {previousPic}></i>
+          <i className="fa-thin fa-arrow-right arrows" onClick= {nextPic}></i>
+        </div>
+      </div>
+    )
+  }
 
 export default Showcase
