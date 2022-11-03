d10399a76b99dce278147f0b89350f00f6523584
timrusin <timrusin@gmail.com>
2022-11-02 20:22:57 -0500

    giving up on random tilt for now, also made before after images slightly smaller to accomodae wider aspect ratios on ldesktop

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -9,11 +9,6 @@ const Services = () => {
   const [cardThree, setCardThree]= useState(false)
   const [cardFour, setCardFour]= useState(false)
 
-  const randomDegree1 = Math.floor(Math.random()* (365 - 0)+ 0)
-  const randomDegree2 = Math.floor(Math.random()* (320 - 90)+90)
-  const randomDegree3 = Math.floor(Math.random()* (320 - 90)+90)
-  const randomDegree4 = Math.floor(Math.random()* (320 - 90)+90)
-
   useEffect(()=> {
     const CardTwo = ()=>{
       setCardTwo(true)
@@ -33,7 +28,7 @@ const Services = () => {
   return (
     <div className="services-container">
       <div className="cards">
-        <div className={cardOne ? "card-one" : "card-hide"} style={{transform: `rotate(${randomDegree1}deg)`}}>
+        <div className={cardOne ? "card-one" : "card-hide"}>
           <Ratecard 
             name={ServiceData[0].name}
             subName={ServiceData[0].subName}
@@ -49,7 +44,7 @@ const Services = () => {
             position={ServiceData[0].position}
           />
         </div>
-        <div className={cardTwo ? "card-two" : "card-hide"} style={{transform: `rotate(${randomDegree2}deg)`}}>
+        <div className={cardTwo ? "card-two" : "card-hide"}>
           <Ratecard
             name={ServiceData[1].name}
             subName={ServiceData[1].subName}
@@ -67,7 +62,7 @@ const Services = () => {
             position={ServiceData[1].position}
           />
         </div>
-        <div className={cardThree ? "card-three" : "card-hide"} style={{transform: `rotate(${randomDegree3}deg)`}}>
+        <div className={cardThree ? "card-three" : "card-hide"}>
           <Ratecard name={ServiceData[2].name}
             subName={ServiceData[2].subName}
             bulletOne={ServiceData[2].bulletOne}
@@ -81,7 +76,7 @@ const Services = () => {
             position={ServiceData[2].position}
            />
         </div>
-        <div className={cardFour ? "card-four" : "card-hide"} style={{transform: `rotate(${randomDegree4}deg)`}}>
+        <div className={cardFour ? "card-four" : "card-hide"}>
           <Ratecard name={ServiceData[3].name}
           bulletOne={ServiceData[3].bulletOne}
           bulletTwo={ServiceData[3].bulletTwo}
