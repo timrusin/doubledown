405780aa1c712af4c28b2d58d6c601f72795f16f
Timothy Rusin <94573769+timrusin@users.noreply.github.com>
2022-11-11 15:56:14 -0600

    Adding useParams
    Adding useParams and passing the parameter through as props to the ContactForm.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import { useParams } from 'react-router-dom';
 import ContactForm from '../components/ContactForm';
 import './Contact.css'
 
 const Contact = () => {
+  
+  const serviceName = useParams()
+  
   return (
     <div className="contact-container fade">
       <h1>Request a free quote</h1>
-      <ContactForm/>
+      <ContactForm serviceName = {serviceName}/>
       <p>*Paint correction refers to the polishing process of removing light surface scratches and restoring the gloss appearence of the paint.</p>
       <p>Oftentimes micro swirling and paint defects are a result of brush car washes.</p>
     </div>
